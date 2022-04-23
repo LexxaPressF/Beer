17a16942989edd21433688e04898fb27e941be3b
@_@/ <57321062+LexxaPressF@users.noreply.github.com>
2022-04-23 15:27:15 +0300

    Modified avatar

--- src/components/Account.vue
+++ src/components/Account.vue
@@ -1,11 +1,7 @@
 <template>
     <div id="acc_bg">
-        <div id="avatar" v-bind:style="{
-            background: `url(${avatar}`,
-            backgroundColor: 'rgba(94, 64, 34, 0.5)',
-            backgroundPosition: 'center',
-            backgroundSize: 'contain',
-            backgroundRepeat: 'no-repeat'}">
+        <div id="avatar">
+            <img :src="avatar" @error="setNone"/>
         </div>
         <div id="wrapper">
             <State v-for="item in Object.keys(accInfo)" :key= "item"
@@ -31,17 +27,23 @@
                 avatar: ''
             }
         },
+        methods:{
+            setNone(e){
+                e.target.src = require("../assets/none.png");
+                console.log('error')
+            }
+        },
         beforeCreate() {
             this.axios.get('https://random-data-api.com/api/users/random_user')
                 .then((response) => {
                     console.log(response.data)
+                    this.avatar = response.data.avatar
                     this.accInfo.name = response.data.first_name + " " + response.data.last_name
                     const birth_date = new Date(response.data.date_of_birth.split("-"))
                     const diff = Date.now() - birth_date.getTime()
                     this.accInfo.age = `${Math.floor(diff / (1000 * 60 * 60 * 24 * 365.25))}`
                     this.accInfo.position = response.data.employment.title
                     this.accInfo.gender = response.data.gender
-                    this.avatar = response.data.avatar
             })
         }
     }
@@ -58,10 +60,19 @@
 }
 
 #avatar{
-    flex-grow: 4;
+    flex-grow: 1;
     border-radius: 16px;
     backdrop-filter: blur(3px);
     box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+    background-color: rgba(94, 64, 34, 0.6);
+    display: flex;
+    justify-content: center;
+    place-items: center;
+}
+
+#avatar > img{
+    height: 350px;
+    /*image-*/
 }
 
 #wrapper{
